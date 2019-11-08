d39d8a23c9a2566f96dfb55c68ca75bf1821473c
ErinMitt <33756931+ErinMitt@users.noreply.github.com>
2019-11-07 20:55:47 -0500

    red colors

---
+++ Escape Room/Assets/triggerComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerComponent : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        print(other.gameObject.name + " has entered the trigger.");
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Escape Room/Assets/turngreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turngreen : MonoBehaviour
+{
+    GameObject ball;
+    GameObject capsuleCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void updateColor()
+    {
+    }
+}
